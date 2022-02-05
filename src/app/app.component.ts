12bbcd4f93b06c145e9bfe2a9f3e6c181820bc82
ALexander Davis <ajdavis2424@gmail.com>
2022-02-05 14:42:27 -0500

    added event handling for buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// State of app lives here, As Well as Methods!
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  // store state of app here
+  newMemberName ="";
+  members:string [] =[];
+
+onInput(member:string){
+this.newMemberName=member;
+console.log(this.newMemberName);
+}
+
+
+  addMember(){
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
 }
