9f66906b716d388c2c327436b890fc3cef4277cc
ALexander Davis <ajdavis2424@gmail.com>
2022-02-06 19:30:46 -0500

    adding logic to make totally functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // store state of app here
-  newMemberName ="";
-  members:string [] =[];
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | "" = "";
+  teams:string [][] = []
 
-onInput(member:string){
-this.newMemberName=member;
-console.log(this.newMemberName);
-}
-
-
-  addMember(){
-    this.members.push(this.newMemberName);
-    console.log(this.members);
+  onInput(member: string) {
+    this.newMemberName = member;
+    // console.log(this.newMemberName);
   }
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty!";
+      return;
+    }
+    this.errorMessage = "Name can't be empty";
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+    // console.log(this.members);
+  }
+
+  generateTeams(){
+    if(!this.numberOfTeams || this.numberOfTeams <=0){
+      return
+    }
+    
+// allMembers copies member state
+    const allMembers = [...this.members]
+
+    for(let i = 0; i < this.numberOfTeams;i++){
+    const randomIndex=Math.floor(Math.random() * allMembers.length)
+    const member = allMembers.splice(randomIndex, 1)[0];
+
+    if(this.teams[i]){
+      this.teams[i].push(member)
+    } else{
+      this.teams[i] = [member]
+    }
+  }
+}
 }
